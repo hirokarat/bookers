f0f06e5b201ef7f7b5d9b988ac6d4f63894c7004
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-07-09 15:22:22 +0000

    make_flash_and_validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,10 +8,11 @@ class BooksController < ApplicationController
   end
   
   def create
-    book=Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id),notice:'Book was successfully created.'
+    @book=Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id),notice:'Book was successfully created.'
     else
+      @books=Book.all
       render:index
     end
   end
@@ -25,9 +26,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id),notice:'Book was successfully updated.'
+    else
+      render:edit 
+    end
   end
   
   def destroy
